2299040a2866a821e56d2a1a42ec6eec86986396
peterChain <80506781+peterChain7@users.noreply.github.com>
2021-08-24 00:39:20 -0700

    Update connection.php

--- StockmanagementSystem/manager/dbconnect/connection.php
+++ StockmanagementSystem/manager/dbconnect/connection.php
@@ -1,1 +1,16 @@
 
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'stock_management';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+if(! $conn )
+ {
+   die('Could not connect: ' . mysqli_error());
+}
+else
+{
+    //echo 'welcome';
+}
+?>
